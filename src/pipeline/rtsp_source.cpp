5c578a2e05c89c3bc70f21f5125a9ce6cd9526db
Gokul-Eswar <gokuleswar177@gmail.com>
2026-02-04 19:54:53 +0530

    going fast

--- src/pipeline/rtsp_source.cpp
+++ src/pipeline/rtsp_source.cpp
@@ -27,13 +27,14 @@ std::string RTSPSource::getPipelineString() const {
     bool nvidia_hw = (use_nv && std::string(use_nv) == "1");
 
     if (nvidia_hw) {
-        pipeline = "rtspsrc location=" + uri_ + " latency=100 drop-on-latency=true ! "
+        // Ultra-Low Latency Settings (latency=0)
+        pipeline = "rtspsrc location=" + uri_ + " latency=0 drop-on-latency=true ! "
                    "rtph264depay ! h264parse ! nvv4l2decoder ! "
                    "nvvideoconvert ! video/x-raw,format=BGR ! "
                    "queue max-size-buffers=1 leaky=2 ! "
                    "appsink name=mysink emit-signals=true max-buffers=1 drop=true";
     } else {
-        pipeline = "rtspsrc location=" + uri_ + " latency=100 drop-on-latency=true ! "
+        pipeline = "rtspsrc location=" + uri_ + " latency=0 drop-on-latency=true ! "
                    "rtph264depay ! h264parse ! decodebin ! "
                    "videoconvert ! video/x-raw,format=BGR ! "
                    "queue max-size-buffers=1 leaky=2 ! "
