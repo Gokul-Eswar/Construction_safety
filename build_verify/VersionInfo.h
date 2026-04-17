6905ca675b312d46e8914f850a60a23dd165d842
Gokul-Eswar <gokuleswar177@gmail.com>
2026-04-17 19:32:30 +0530

    Refactor code structure for improved readability and maintainability

---
+++ build_verify/VersionInfo.h
@@ -0,0 +1,7 @@
+#ifndef VERSIONINFO_H
+#define VERSIONINFO_H
+
+#define BUILD_TIMESTAMP "2026-04-17T13:50:11Z"
+#define CLIENT_VERSION  "1.3.13"
+
+#endif /* VERSIONINFO_H */
---
+++ build_verify/_deps/pahomqttc-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/GOKUL_ESWAR/Codebase/construction safety/build_verify/_deps/pahomqttc-src
+# Build directory: D:/GOKUL_ESWAR/Codebase/construction safety/build_verify/_deps/pahomqttc-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
